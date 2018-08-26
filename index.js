c2af4cb51d7b7883b7f89f257ca6b4aaac006911
Anuj Panchal <anujpan1@gmail.com>
2018-08-26 02:27:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(days, holiday) {
+function holidayCountdown(holidaym days) {
   return `It's ${days} days until ${holiday}!`
 }
